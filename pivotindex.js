381f2a4359faba4e4dcb12c8059dabb1dc6d4afc
vikas-tiwari93 <vikastiwarisync@gmail.com>
2023-05-13 10:17:13 +0530

    first commit

---
+++ pivotindex.js
@@ -0,0 +1,23 @@
+let arr = [1, 2, 3, 4, 5, 6, 7, 8];
+function pivot(input) {
+  if (Array.isArray(input) && input.length > 2) {
+    let sum = 0;
+
+    for (let i = 0; i < input.length; i++) {
+      sum += input[i];
+    }
+    let leftsum = 0;
+    let rightsum = sum;
+    for (let j = 0; j < input.length; j++) {
+      if (leftsum !== rightsum) {
+        leftsum = leftsum + input[j - 1] || 0;
+        rightsum = rightsum - input[j];
+      } else if (leftsum === rightsum) {
+        return j;
+      } else {
+        return console.log("no pivot index found");
+      }
+    }
+  } else console.log("enter a valid array oinput");
+}
+console.log(pivot(arr));
