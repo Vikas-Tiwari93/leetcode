2f096bccc3831847a81d32b67853593de0a492f5
vikas-tiwari93 <vikastiwarisync@gmail.com>
2023-05-23 15:01:07 +0530

    again

--- 4a2c1b2a.js
+++ 4a2c1b2a.js
@@ -1,5 +1,14 @@
-
-
-
-
-curry(1)(2)(3)(4)
+function stringcount(input) {
+  let count = 1;
+  let string = "";
+  for (let i = 0; i < input.length; i++) {
+    if (input[i] === input[i + 1]) {
+      count += 1;
+    } else {
+      string = string + input[i] + count;
+      count = 1;
+    }
+  }
+  console.log(string);
+}
+stringcount("aaaabbbaacccc");
